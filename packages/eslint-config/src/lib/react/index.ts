a26c1d96e05024d523fad6c6354a072cccb6a305
Act-Aks <akashsrivastava.git@gmail.com>
2025-04-06 12:14:44 +0530

    refactor: remove @eslint/eslintrc dependency and simplify react config
    The @eslint/eslintrc dependency was removed as it is no longer needed. Additionally, the react configuration was simplified by directly using eslint-plugin-react-hooks instead of relying on FlatCompat. This improves maintainability and reduces unnecessary dependencies.

--- packages/eslint-config/src/lib/react/index.ts
+++ packages/eslint-config/src/lib/react/index.ts
@@ -1,13 +1,11 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import reactHooks from 'eslint-plugin-react-hooks'
 import { ConfigArray } from 'typescript-eslint'
 
 import { customReact } from './customReact'
 import { noDefaultReactImportInSpecs } from './noDefaultReactImportInSpecs'
 
-const compat = new FlatCompat()
-
 export const reactEslintConfig: ConfigArray = [
-    ...compat.extends('plugin:react-hooks/recommended'),
+    reactHooks.configs['recommended-latest'],
     customReact,
     noDefaultReactImportInSpecs,
 ]
